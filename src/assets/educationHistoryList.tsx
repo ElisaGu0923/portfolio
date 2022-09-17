59b2ca84397769fbb4dc99e2a051e8ed2b743ab6
Elisa <elisa@cloudsparklabs.com>
2022-09-17 14:38:54 -0400

    +

--- src/assets/educationHistoryList.tsx
+++ src/assets/educationHistoryList.tsx
@@ -2,7 +2,7 @@ import { Experience } from "../components/history/history"
 
 export const educationHistoryList: Experience[] = [
     { name: "University of Waterloo", title: "Student", startDate: new Date('September, 2020'), endDate: new Date('January, 2025'), description: ["Computer Science (co-op)"] },
-    { name: "Object-Oriented Software Development", startDate: new Date('May, 2021'), endDate: new Date('August, 2021'), description: ["Designed, coded, debugged, tested, and documented medium-sized programs in C++"] },
+    { name: "Object-Oriented Software Development", startDate: new Date('May, 2021'), endDate: new Date('August, 2021'), description: ["Coded, documented, debugged, and created test suites for medium-sized C++ programs", "Studied and implemented multiple design patterns", "Analysed and designed UML diagrams"] },
     { name: "Data Structures and Data Management", startDate: new Date('January, 2022'), endDate: new Date('April, 2022'), description: ["Gained understanding of priority queues, sorting, dictionaries, data structures for text processing"] },
     { name: "Algorithms", startDate: new Date('September, 2022'), endDate: new Date('December, 2022'), description: ["Learning divide and conquer algorithms, recurrences, greedy algorithms, dynamic programming, graph search and backtrack"] },
     { name: "Application Development", startDate: new Date('September, 2022'), endDate: new Date('December, 2022'), description: ["Working in project teams to design and build complete, working applications and services with Kotlin and Gradle", "Created prototypes of software using Figma"] },
