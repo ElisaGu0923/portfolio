17c396999d39d8451d5cf5257978aedb82efa8af
Elisa <elisa@cloudsparklabs.com>
2022-09-17 23:19:04 -0400

    +

--- src/assets/workHistoryList.tsx
+++ src/assets/workHistoryList.tsx
@@ -1,6 +1,6 @@
 import { Experience } from "../components/history/history"
 
 export const workHistoryList: Experience[] = [
-    { name: "Sun Life Financial", title: "API Developer", startDate: new Date('May, 2022'), endDate: new Date('August, 2022'), evaluation: "Outstanding", description: [""], file: "https://drive.google.com/file/d/1TATciHTyGY2V5qcJdlYk_JyWF4PJ678C/view?usp=sharing" },
-    { name: "CloudSpark", title: "Product Developer Assistant", startDate: new Date('September, 2020'), endDate: new Date('January, 2025'), evaluation: "Excellent", description: ["sdijfisjf", "fdkslajfks"], file: "https://drive.google.com/file/d/1gDp7JPy5mxgx08FbjhJcltXOS5GsTTP6/view?usp=sharing" },
+    { name: "Sun Life Financial", title: "API Developer", startDate: new Date('May, 2022'), endDate: new Date('August, 2022'), evaluation: "Outstanding", description: [], file: "https://drive.google.com/file/d/1TATciHTyGY2V5qcJdlYk_JyWF4PJ678C/view?usp=sharing" },
+    { name: "CloudSpark", title: "Product Developer Assistant", startDate: new Date('September, 2020'), endDate: new Date('January, 2025'), evaluation: "Excellent", description: [], file: "https://drive.google.com/file/d/1gDp7JPy5mxgx08FbjhJcltXOS5GsTTP6/view?usp=sharing" },
 ]
